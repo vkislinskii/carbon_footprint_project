ace2c2d316f0f75eb38ec20e25e94f1b5fe7e8b8
vkislinskii <158157167+vkislinskii@users.noreply.github.com>
2024-11-12 13:17:27 +0100

    small description improvements

---
+++ Introduction.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.write('''#### Welcome to the carbon footprint calculator.''')
+
+with st.expander("Why does my company need this?"):
+    st.write("It's important due to several social, regulatory, and economic factors:")
+    st.write('###### 1. Regulatory Compliance')
+    st.write("Germany, as part of the European Union, has strong regulations and ambitious goals for reducing greenhouse gas emissions.")
+    st.write('''EU regulations, like the [Corporate Sustainability Reporting Directive (CSRD)](https://finance.ec.europa.eu/capital-markets-union-and-financial-markets/company-reporting-and-auditing/company-reporting/corporate-sustainability-reporting_en), 
+             require companies to disclose their environmental impact, including their carbon emissions.
+             ''')
+    st.write('###### 2. Consumer Demand')
+    st.write("Consumers in Germany are highly eco-conscious, so reducing emissions can enhance customer loyalty and brand image.")
+    st.write('###### 3. Competitive Advantage')
+    st.write("Companies that demonstrate strong environmental credentials attract eco-conscious investors, partners and talent.")
+    st.write('###### 4. Cost Savings')
+    st.write("Measuring carbon emissions helps companies reveal inefficiencies and wasteful practices, leading to cost-saving opportunities.")
+
+st.write('''Ready for the action?\nLet's calculate your company's carbon footprint!''')
+st.page_link("pages/Questions.py", label='Go to questions', icon="😍")
--- pages/Questions.py
+++ pages/Questions.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit.components.v1 as components
 import sqlite3
 from datetime import datetime
 import pandas as pd
@@ -7,25 +8,29 @@ import pandas as pd
 st.text_input("Your company's name", key="company_name")
 
 with st.expander("Energy Usage", expanded=True):
-    electricity_bill = st.number_input("What is your average monthly electricity bill in euros?", min_value=0, value=100)
-    gas_bill = st.number_input('What is your average monthly gas bill in euros?', min_value=0, value=100)
-    fuel_bill = st.number_input('What is your average monthly fuel bill for transportation in euros?', min_value=0, value=100)
+    electricity_bill = st.number_input("What is your average monthly electricity bill in euros?", min_value=0, value=None, placeholder="Type a number...")
+    gas_bill = st.number_input('What is your average monthly gas bill in euros?', min_value=0, value=None, placeholder="Type a number...")
+    fuel_bill = st.number_input('What is your average monthly fuel bill for transportation in euros?', min_value=0, value=None, placeholder="Type a number...")
 
-    energy_usage = round((electricity_bill * 12 * 0.0005) + (gas_bill * 12 * 0.0053) + (fuel_bill * 12 * 2.32), 2)
-    st.write("#### Energy's part in your carbot footprint is ", energy_usage, "kgCO2")
+    if pd.isna(electricity_bill) + pd.isna(gas_bill) + pd.isna(fuel_bill) == False:
+        energy_usage = round((electricity_bill * 12 * 0.0005) + (gas_bill * 12 * 0.0053) + (fuel_bill * 12 * 2.32), 2)
+        st.write("#### Energy's part in your carbot footprint is ", energy_usage, "kgCO2")
 
 with st.expander("Waste", expanded=True):
-    waste_amt = st.number_input('How much waste do you generate per month in kilograms?', min_value=0, value=50)
+    waste_amt = st.number_input('How much waste do you generate per month in kilograms?', min_value=0, value=None, placeholder="Type a number...")
     recycled_prc = st.slider('How much of that waste is recycled or composed (in percentage)?', 0, 100, 20)
 
-    waste = round(waste_amt * 12 * (0.57-recycled_prc/100), 2)
-    st.write("#### Waste's part in your carbot footprint is", waste, "kgCO2")
+    if pd.isna(waste_amt) == False:
+        waste = round(waste_amt * 12 * (0.57-recycled_prc/100), 2)
+        st.write("#### Waste's part in your carbot footprint is", waste, "kgCO2")
 
 with st.expander("Business Travel", expanded=True):
-    travel_km = st.number_input('How many kilometers do your employees travel per year for business purposes?', min_value=0, value=100)
+    travel_km = st.number_input('How many kilometers do your employees travel per year for business purposes?', min_value=0, value=None, placeholder="Type a number...")
     fuel_eff = st.slider('What is the average fuel efficiency of the vehicles used for business travel in liters per 100 kilometers?', 0, 25, 7)
-    travel = round(travel_km * 1/fuel_eff * 2.31, 2)
-    st.write("#### Travel's part in your carbot footprint is ", travel, "kgCO2")
+
+    if pd.isna(travel_km) == False:
+        travel = round(travel_km * 1/fuel_eff * 2.31, 2)
+        st.write("#### Travel's part in your carbot footprint is ", travel, "kgCO2")
 
 response_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
@@ -75,24 +80,20 @@ def get_user_value(type):
     conn.close()  
     return round(result[0], 2) if result else None 
 
-#3. creating dataframe with entered data
-chart_data = pd.DataFrame(
-    {
-        "Comparison": [st.session_state.company_name, "Average"],
-        "Energy Usage": [energy_usage, get_user_value('energy')],
-        "Waste": [waste, get_user_value('waste')],
-        "Business Travel": [travel, get_user_value('travel')]
-    }
-    )
 
-#4. submitting user's data, visualizing it and comparing it with our database's average
-if st.button('Submit my data', type='primary') and st.session_state.company_name:
+if st.button('Submit my data', type='primary') and st.session_state.company_name and pd.isna(electricity_bill) == False:
+    #3. creating dataframe with entered data
+    chart_data = pd.DataFrame(
+        {
+            "Comparison": [st.session_state.company_name, "Average"],
+            "Energy Usage": [energy_usage, get_user_value('energy')],
+            "Waste": [waste, get_user_value('waste')],
+            "Business Travel": [travel, get_user_value('travel')]
+        }
+        )
+
+    #4. submitting user's data, visualizing it and comparing it with our database's average
     save_user_value(st.session_state.company_name, electricity_bill, gas_bill, fuel_bill, waste_amt, recycled_prc, travel_km, fuel_eff, response_date)
 
     chart_data
     st.bar_chart(chart_data, x="Comparison", y=["Energy Usage", "Waste", "Business Travel"], stack=False)
-
-
-
-
-
