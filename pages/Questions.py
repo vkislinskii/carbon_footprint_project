952ae5c372171b9faacc3790c94afc6a2493b6bb
vkislinskii <158157167+vkislinskii@users.noreply.github.com>
2024-12-08 19:22:23 +0100

    Optimized code

--- pages/Questions.py
+++ pages/Questions.py
@@ -5,143 +5,59 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-st.subheader('Questionnaire', divider="gray")
-#1. getting information from the user
-st.text_input("Your company's name", key="company_name")
+#0. Defining the functions
+def get_user_inputs():
+    company_name = st.text_input("Your company's name", key="company_name")
+    with st.expander("Energy Usage Questions", expanded=True):
+        electricity = st.number_input("What is your average monthly electricity bill in euros?", min_value=0, value=None, placeholder="Type a number...")
+        gas = st.number_input('What is your average monthly gas bill in euros?', min_value=0, value=None, placeholder="Type a number...")
+        fuel = st.number_input('What is your average monthly fuel bill for transportation in euros?', min_value=0, value=None, placeholder="Type a number...")
 
-with st.expander("Energy Usage Questions", expanded=True):
-    electricity_bill = st.number_input("What is your average monthly electricity bill in euros?", min_value=0, value=None, placeholder="Type a number...")
-    gas_bill = st.number_input('What is your average monthly gas bill in euros?', min_value=0, value=None, placeholder="Type a number...")
-    fuel_bill = st.number_input('What is your average monthly fuel bill for transportation in euros?', min_value=0, value=None, placeholder="Type a number...")
+    with st.expander("Waste Questions", expanded=True):
+        waste_amt = st.number_input('How much waste do you generate per month in kilograms?', min_value=0, value=None, placeholder="Type a number...")
+        recycled_prc = st.slider('How much of that waste is recycled or composed (in percentage)?', 0, 100, 20)
 
-    if pd.isna(electricity_bill) + pd.isna(gas_bill) + pd.isna(fuel_bill) == False:
-        energy_usage = round((electricity_bill * 12 * 0.0005) + (gas_bill * 12 * 0.0053) + (fuel_bill * 12 * 2.32), 2)
-        st.write("#### Energy's part in your carbot footprint is ", energy_usage, "kgCO2")
+    with st.expander("Business Travel Questions", expanded=True):
+        travel_km = st.number_input('How many kilometers do your employees travel per year for business purposes?', min_value=0, value=None, placeholder="Type a number...")
+        fuel_eff = st.slider('What is the average fuel efficiency of the vehicles used for business travel in liters per 100 kilometers?', 1, 25, 7)
 
-with st.expander("Waste Questions", expanded=True):
-    waste_amt = st.number_input('How much waste do you generate per month in kilograms?', min_value=0, value=None, placeholder="Type a number...")
-    recycled_prc = st.slider('How much of that waste is recycled or composed (in percentage)?', 0, 100, 20)
+    return company_name, electricity, gas, fuel, waste_amt, recycled_prc, travel_km, fuel_eff
 
-    if pd.isna(waste_amt) == False:
-        waste = round(waste_amt * 12 * (0.57-recycled_prc/100), 2)
-        st.write("#### Waste's part in your carbot footprint is", waste, "kgCO2")
+def calculate_emissions(electricity, gas, fuel, waste_amt, recycled_prc, travel_km, fuel_eff):
+    energy_usage = round((electricity * 12 * 0.0005) + (gas * 12 * 0.0053) + (fuel * 12 * 2.32), 2)
+    waste = round(waste_amt * 12 * (0.57-recycled_prc/100), 2)
+    travel = round(travel_km * fuel_eff * 2.31, 2)
+    return energy_usage, waste, travel
 
-with st.expander("Business Travel Questions", expanded=True):
-    travel_km = st.number_input('How many kilometers do your employees travel per year for business purposes?', min_value=0, value=None, placeholder="Type a number...")
-    fuel_eff = st.slider('What is the average fuel efficiency of the vehicles used for business travel in liters per 100 kilometers?', 1, 25, 7)
-
-    if pd.isna(travel_km) == False:
-        travel = round(travel_km * fuel_eff * 2.31, 2)
-        st.write("#### Travel's part in your carbot footprint is ", travel, "kgCO2")
-
-response_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-#2. defining functions for saving and getting user's data
-def save_user_value(company_name, electricity_bill, gas_bill, fuel_bill, waste_amt, recycled_prc, travel_km, fuel_eff, response_date, energy_usage, waste, travel):
+def save_user_value(company_name, electricity, gas, fuel, waste_amt, recycled_prc, travel_km, fuel_eff, response_date, energy_usage, waste, travel):
     conn = sqlite3.connect("data.db")  
     cursor = conn.cursor()
     cursor.execute('''INSERT OR REPLACE INTO results_raw VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)''', 
-                   (company_name, electricity_bill, gas_bill, fuel_bill, waste_amt, recycled_prc, travel_km, fuel_eff, response_date))
+                   (company_name, electricity, gas, fuel, waste_amt, recycled_prc, travel_km, fuel_eff, response_date))
     cursor.execute('''INSERT OR REPLACE INTO results_summary VALUES (?, ?, ?, ?, ?)''', 
                    (company_name, energy_usage, waste, travel, response_date))
     conn.commit()  
     conn.close()   
 
-def get_values(type):
-    conn = sqlite3.connect("data.db")  
-    cursor = conn.cursor()
-    if type == 'energy':
-        cursor.execute('''  SELECT AVG(energy_usage_emission)
-                        FROM    (SELECT
-                                    energy_usage_emission
-                                FROM results_summary
-                                ORDER BY energy_usage_emission
-                                LIMIT 2 - (SELECT COUNT(*) FROM results_summary) % 2 -- odd 1, even 2
-                                OFFSET (SELECT (COUNT(*) - 1) / 2
-                                        FROM results_summary))    
-                   ''')
-    elif type == 'waste':
-        cursor.execute('''  SELECT AVG(waste_emission)
-                        FROM    (SELECT
-                                    waste_emission
-                                FROM results_summary
-                                ORDER BY waste_emission
-                                LIMIT 2 - (SELECT COUNT(*) FROM results_summary) % 2 -- odd 1, even 2
-                                OFFSET (SELECT (COUNT(*) - 1) / 2
-                                        FROM results_summary))    
-                   ''')
-    elif type == 'travel':
-        cursor.execute('''  SELECT AVG(travel_emission)
-                        FROM    (SELECT
-                                    travel_emission
-                                FROM results_summary
-                                ORDER BY travel_emission
-                                LIMIT 2 - (SELECT COUNT(*) FROM results_summary) % 2 -- odd 1, even 2
-                                OFFSET (SELECT (COUNT(*) - 1) / 2
-                                        FROM results_summary))    
-                   ''')
-    result = cursor.fetchone()     
-    conn.close()  
-    return round(result[0], 2) if result else None 
+def get_values(db_path="data.db"):
+    query = ''' SELECT 
+                    energy_usage_emission,
+                    waste_emission,
+                    travel_emission
+                FROM results_summary'''
+    with sqlite3.connect(db_path) as conn:
+        data = pd.read_sql(query, conn)
+    return data  
 
-check_variable = pd.isna(electricity_bill) + pd.isna(gas_bill) + pd.isna(fuel_bill) + pd.isna(waste_amt) + pd.isna(travel_km)
-
-if st.button('Submit my data', type='primary') and st.session_state.company_name and check_variable == False:
-    #3. creating dataframe with entered data
-    total = round(energy_usage + waste + travel, 2)
-    energy_usage_median = get_values('energy')
-    waste_median = get_values('waste')
-    travel_median = get_values('travel')
-    total_median = round(energy_usage_median + waste_median + travel_median, 2)
-
-    energy_usage_diff = str(round((energy_usage / energy_usage_median - 1) * 100, 2)) + " %"
-    waste_diff = str(round((waste / waste_median - 1) * 100, 2)) + " %"
-    travel_diff = str(round((travel / travel_median - 1) * 100, 2)) + " %"
-    total_diff = str(round((total / total_median - 1) * 100, 2)) + " %"
-
-    chart_data = pd.DataFrame(
-        {
-            "Comparison": [st.session_state.company_name, "Median", "Difference"],
-            "Energy Usage": [str(energy_usage), str(energy_usage_median), energy_usage_diff],
-            "Waste": [str(waste), str(waste_median), waste_diff],
-            "Business Travel": [str(travel), str(travel_median), travel_diff],
-            "Total": [str(total), str(total_median), total_diff]
-        }
-        )
-
-    def highlight_row(row):
-        if row['Comparison'] == 'Difference':  # Highlight row with index 1
-            return ['background-color: lightsteelblue'] * len(row)
-        else:
-            return [''] * len(row)
-
-
-    #4. submitting user's data, visualizing it and comparing it with our database's average
-    save_user_value(st.session_state.company_name, electricity_bill, gas_bill, fuel_bill, waste_amt, recycled_prc, travel_km, fuel_eff, response_date, energy_usage, waste, travel)
-
-    st.subheader("Comparison of yours and other companies' carbon footprints", divider="gray")
-    #st.write("##### Comparison of yours and other companies' carbon footprints")
-    st.write("###### Absolute values")
-
-    chart_data = chart_data.style.apply(highlight_row, axis=1)
-    st.dataframe(chart_data, hide_index=True)
-    
-    st.write("###### Shares of carbon footprint components")
-    labels = ['Energy Usage', 'Waste', 'Business Travel'] #chart_data.iloc[0] #chart_data['Comparison'] #'Energy Usage', 'Hogs', 'Dogs', 'Logs'
-    sizes = [energy_usage, waste, travel]
-    sizes_median = [energy_usage_median, waste_median, travel_median]
-
-    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
-    axes[0].pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90, colors=sns.color_palette('coolwarm'))
-    axes[0].set_title(f"{st.session_state.company_name}")
-    #ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-    
-    axes[1].pie(sizes_median, labels=labels, autopct='%1.1f%%', startangle=90, colors=sns.color_palette('coolwarm'))
+def generate_chart(data0, data1, labels, company_name):
+    fig, axes = plt.subplots(1, 2, figsize=(10, 6))
+    axes[0].pie(data0, labels=labels, autopct='%1.1f%%', startangle=90, colors=sns.color_palette('coolwarm'))
+    axes[0].set_title(company_name)   
+    axes[1].pie(data1, labels=labels, autopct='%1.1f%%', startangle=90, colors=sns.color_palette('coolwarm'))
     axes[1].set_title("Median values for all our partner companies")
-    
-    st.pyplot(fig)
+    return fig
 
-    st.write("##### Recommendations")    
+def display_recommendations(energy_usage, waste, travel):
     if energy_usage <= 5000:
         st.write(f"**You have low energy related emissions, {energy_usage} kgCO2.**")
         st.write("*Great job!* Your energy consumption is efficient. Maintain current practices by conducting periodic energy audits to ensure no inefficiencies creep in.")
@@ -163,7 +79,7 @@ if st.button('Submit my data', type='primary') and st.session_state.company_name
             "Right limit, kgCO2": ["5000", "15000", "∞"]
         }
         )
-    st.dataframe(energy_df, hide_index=True) 
+    st.dataframe(energy_df, hide_index=True)
 
     if waste <= 1000:
         st.write(f"**You have low waste related emissions, {waste} kgCO2.**")
@@ -211,3 +127,70 @@ if st.button('Submit my data', type='primary') and st.session_state.company_name
         }
         )
     st.dataframe(travel_df, hide_index=True)
+
+
+st.subheader('Questionnaire', divider="gray")
+
+#1. getting information from the user
+company_name, electricity, gas, fuel, waste_amt, recycled_prc, travel_km, fuel_eff = get_user_inputs()
+
+if st.button("Submit Data"):
+    if company_name:
+        #2. Calculating emissions of the company
+        energy_usage, waste, travel = calculate_emissions(electricity, gas, fuel, waste_amt, recycled_prc, travel_km, fuel_eff)
+
+        #3. Saving the values 
+        response_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        save_user_value(company_name, electricity, gas, fuel, waste_amt, recycled_prc, travel_km, fuel_eff, response_date, energy_usage, waste, travel)
+
+        #4. Table data
+        st.subheader("Comparison of yours and other companies' carbon footprints", divider="gray")
+        st.write("###### Absolute values")
+        total = round(energy_usage + waste + travel, 2)
+        data = get_values()
+        energy_usage_median = data['energy_usage_emission'].median()
+        waste_median = data['waste_emission'].median()
+        travel_median = data['travel_emission'].median()
+        total_median = round(energy_usage_median + waste_median + travel_median, 2)
+
+        energy_usage_diff = str(round((energy_usage / energy_usage_median - 1) * 100, 2)) + " %"
+        waste_diff = str(round((waste / waste_median - 1) * 100, 2)) + " %"
+        travel_diff = str(round((travel / travel_median - 1) * 100, 2)) + " %"
+        total_diff = str(round((total / total_median - 1) * 100, 2)) + " %"
+
+        chart_data = pd.DataFrame(
+            {
+                "Comparison": [st.session_state.company_name, "Median", "Difference"],
+                "Energy Usage": [str(energy_usage), str(energy_usage_median), energy_usage_diff],
+                "Waste": [str(waste), str(waste_median), waste_diff],
+                "Business Travel": [str(travel), str(travel_median), travel_diff],
+                "Total": [str(total), str(total_median), total_diff]
+            }
+            )
+
+        def highlight_row(row):
+            if row['Comparison'] == 'Difference':  
+                return ['background-color: lightsteelblue'] * len(row)
+            else:
+                return [''] * len(row)
+            
+        chart_data = chart_data.style.apply(highlight_row, axis=1)
+        st.dataframe(chart_data, hide_index=True)
+        
+        #5. Pie chart
+        sizes = [energy_usage, waste, travel]
+        sizes_median = [energy_usage_median, waste_median, travel_median]
+        fig = generate_chart(sizes, sizes_median, ["Energy Usage", "Waste", "Business Travel"], st.session_state.company_name)
+        st.pyplot(fig)
+
+        #6. Recommendations
+        st.write("##### Recommendations")
+        display_recommendations(energy_usage, waste, travel)
+
+
+
+
+    
+
+
+    #4. submitting user's data, visualizing it and comparing it with our database's average
--- pages/Summary.py
+++ pages/Summary.py
@@ -1,18 +1,14 @@
 import streamlit as st
 import sqlite3
-import pandas as pd
-import plotly.figure_factory as ff 
+import pandas as pd 
 import seaborn as sns
 import matplotlib.pyplot as plt
-import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
 
-#0. getting the data
-def get_data_all_users():
-    conn = sqlite3.connect("data.db")  
-    cursor = conn.cursor()    
+#0. Defining the functions: to get data, to set up visualizations
+def get_data_all_users(db_path="data.db"):
     query = f'''SELECT 
                     company_name,
                     energy_usage_emission,
@@ -21,90 +17,85 @@ def get_data_all_users():
                     energy_usage_emission + waste_emission + travel_emission as overall_emission,
                     date(response_date) as response_date 
                 FROM results_summary'''
-    data = pd.read_sql(query, conn)
-    conn.close()
+    with sqlite3.connect(db_path) as conn:
+        data = pd.read_sql(query, conn)
     return data 
 
-data = get_data_all_users()
+def plot_pie_chart(values, labels):
+    def make_autopct(values):
+        def my_autopct(pct):
+            total = sum(values)
+            val = int(round(pct*total/100.0))
+            return '{p:.1f}%  \n({v:d} kgCO2)'.format(p=pct,v=val)
+        return my_autopct                       
+    
+    fig, ax = plt.subplots(figsize=(3, 2.2))
+    ax.pie(
+        values, labels=labels, autopct=make_autopct(values),
+        startangle=90, textprops={'fontsize': 4.6},
+        colors=sns.color_palette('coolwarm')
+    )
+    return fig
+
+def plot_box_charts(data):
+    fig = make_subplots(rows=1, cols=3, horizontal_spacing=0.1)
+    fig.add_trace(go.Box(y=data['energy_usage_emission'], name="Energy kgCO2"), row=1, col=1)
+    fig.add_trace(go.Box(y=data['waste_emission'], name="Waste kgCO2"), row=1, col=2)
+    fig.add_trace(go.Box(y=data['travel_emission'], name="Travel kgCO2"), row=1, col=3)
+    fig.update_layout(height=400, margin=dict(l=20, r=20, t=20, b=20))
+    return fig
+
+def plot_total_emission_distribution(data):
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.histplot(data, x='overall_emission', kde=True, color='lightskyblue')
+    plt.xlabel('Overall Emission')
+    plt.ylabel('Number of Companies')
+    return fig
+
+def plot_total_emission_by_company(data):
+    fig = plt.figure(figsize=(10, 6))
+    sns.barplot(x='company_name', y='overall_emission', data=data, palette='coolwarm', ci=None)
+    plt.xlabel('Company Name')
+    plt.ylabel('Overall Emission')
+    return fig
+
 
 #1. Overview visualizations
 st.subheader('High-Level Overview', divider="gray")
+data = get_data_all_users()
+
 #1.1. Pie chart for the median values
-
-
-energy_usage_median = round(data['energy_usage_emission'].median(), 2) 
-waste_median = round(data['waste_emission'].median(), 2) 
-travel_median = round(data['travel_emission'].median(), 2) 
-labels = ['Energy Usage', 'Waste', 'Business Travel']
-sizes_median = [energy_usage_median, waste_median, travel_median]
-
-def make_autopct(values):
-    def my_autopct(pct):
-        total = sum(values)
-        val = int(round(pct*total/100.0))
-        return '{p:.1f}%  \n({v:d} kgCO2)'.format(p=pct,v=val)
-    return my_autopct
-
-fig, axes = plt.subplots(figsize=(3, 2.2)) 
-axes.pie(sizes_median, labels=labels, autopct=make_autopct(sizes_median), startangle=90, textprops={'fontsize': 4.6 }, colors=sns.color_palette('coolwarm'))
-#plt.tight_layout()
+median_emission_values = [
+    round(data['energy_usage_emission'].median(), 2),
+    round(data['waste_emission'].median(), 2),
+    round(data['travel_emission'].median(), 2)
+]
+pie_chart = plot_pie_chart(median_emission_values, ['Energy Usage', 'Waste', 'Business Travel'])
 
 st.write("###### Median value distribution across partner companies")
 col1, col2, col3 = st.columns([0.25, 3, 0.25])  
 with col2:
-    st.pyplot(fig, use_container_width=False)
-
-
+    st.pyplot(pie_chart, use_container_width=False)
 
 #1.2. Box charts for each emission type
-fig = make_subplots(rows=1, cols=3, horizontal_spacing=0.1)
-fig.add_trace(go.Box(y=data['energy_usage_emission'], name="Energy kgCO2"), row=1, col=1)
-fig.add_trace(go.Box(y=data['waste_emission'], name="Waste kgCO2"), row=1, col=2)
-fig.add_trace(go.Box(y=data['travel_emission'], name="Travel kgCO2"), row=1, col=3)
-fig.update_layout(height=400, margin=dict(l=20, r=20, t=20, b=20))
-
 st.write("###### Distribution by emission type")
-st.plotly_chart(fig)
-
+box_charts = plot_box_charts(data)
+st.plotly_chart(box_charts)
 
 #1.3. Total emission distribution plot 
-fig, ax = plt.subplots(figsize=(12, 6))
-sns.histplot(data, x='overall_emission', kde=True, color='lightskyblue')
-plt.xlabel('Overall emission')
-plt.ylabel('Number of companies')
-
 st.write("###### Distribution of total emission across companies")
-st.pyplot(fig)
+total_emission_distribution = plot_total_emission_distribution(data)
+st.pyplot(total_emission_distribution)
 
-#--------------------------------------
+
 #2. Overview visualizations
 st.subheader('Companies Overview', divider="gray")
 
+#2.1. Table data
 st.write("###### Table with data of all companies")
 st.dataframe(data, hide_index=True)
 
-
-
-
-df_long = pd.melt(data, id_vars=['company_name'], value_vars=['energy_usage_emission', 'waste_emission', 'travel_emission'],
-                  var_name='emission_type', value_name='emission_amount')
-
-df_long2 = df_long
-df_long2['total_emission'] = df_long.groupby('company_name')['emission_amount'].transform('sum')
-
-# Calculate share of each emission type
-df_long2['share'] = df_long['emission_amount'] / df_long['total_emission']
-
-# Sort values to ensure proper stacking order
-df_long2['emission_type'] = pd.Categorical(df_long['emission_type'], 
-                                           categories=['energy_usage_emission', 'waste_emission', 'travel_emission'], 
-                                           ordered=True)
-df_long2 = df_long.sort_values(by=['company_name', 'emission_type'])
-
-
-# 2.2. total emissions by company
+# 2.2. Total emissions by company
 st.write("###### Total emissions per company")
-plt.figure(figsize=(10, 6))
-sns.barplot(x='company_name', y='total_emission', data=df_long, palette='coolwarm', ci=None) #, hue='emission_type'
-#plt.xticks(rotation=45)
-st.pyplot(plt)
+total_emission_by_company = plot_total_emission_by_company(data)
+st.pyplot(total_emission_by_company)
